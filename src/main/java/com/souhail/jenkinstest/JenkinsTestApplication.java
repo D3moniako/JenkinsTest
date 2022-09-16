0a50e8bd117a0003e13f56f252f827598055d8ee
D3moniako <soleroboo@gmail.com>
2022-09-16 16:07:14 +0200

    correzione errore

--- src/main/java/com/souhail/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/souhail/jenkinstest/JenkinsTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class JenkinsTestApplication {
 
-    @GetMapping("/ping")JKKLòOPKKJNJKO
+    @GetMapping("/ping")
     public String message() {
         return "Wao!! Application Deployed successfully in SAP Cloud..";
     }
