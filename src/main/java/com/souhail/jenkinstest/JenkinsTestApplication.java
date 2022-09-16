fc44f10a53dbef7fa101ae76e5eefffe24e26974
D3moniako <soleroboo@gmail.com>
2022-09-16 12:07:13 +0200

    Inizio prova

---
+++ src/main/java/com/souhail/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,18 @@
+package com.souhail.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    @GetMapping("/ping")
+    public String message() {
+        return "Wao!! Application Deployed successfully in SAP Cloud..";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+
+}
