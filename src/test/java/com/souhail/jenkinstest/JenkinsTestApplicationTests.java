97129394f1c0d4e084466dde30cf3e8fc774ef48
D3moniako <soleroboo@gmail.com>
2022-09-19 10:46:48 +0200

    test aggiustato

--- src/test/java/com/souhail/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/souhail/jenkinstest/JenkinsTestApplicationTests.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class JenkinsTestApplicationTests {
 
     @Test
-   public  contextLoads() {
+   public void contextLoads() {
         log.info("Test in esecuzione");
         assertEquals(true,true);
     }
