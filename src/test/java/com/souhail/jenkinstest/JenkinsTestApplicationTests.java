56691c53dbe1ba72005bba7e1be9a9715c80ecae
D3moniako <soleroboo@gmail.com>
2022-09-19 10:27:07 +0200

    AGGIUNTA junit test

--- src/test/java/com/souhail/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/souhail/jenkinstest/JenkinsTestApplicationTests.java
@@ -1,13 +1,22 @@
 package com.souhail.jenkinstest;
 
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)// serve per introdurre jUNIT TEST, CHE HO NEL POM
+@Log
 @SpringBootTest
 class JenkinsTestApplicationTests {
 
     @Test
-    void contextLoads() {
+   public void contextLoads() {
+        log.info("Test in esecuzione");
+        assertEquals(true,true);
     }
 
 }
